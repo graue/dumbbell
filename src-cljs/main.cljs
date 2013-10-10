3eb8ba99f79a83ca0d0a7574e98b12acc2a3a50d
Scott Feeney <scott@oceanbase.org>
2013-10-10 13:23:18 -0400

    save initial game data

--- src-cljs/main.cljs
+++ src-cljs/main.cljs
@@ -1,8 +1,12 @@
 (ns dumbbell.main
   (:require [monet.canvas :as canvas]))
 
+(def ctx (atom nil))
+(def snake-dir (atom :right))
+(def snake-pos (atom {:x 200 :y 200}))
+
 (defn ^:export startgame
   [el]
-  (let [ctx (canvas/get-context el "2d")]
-    (canvas/fill-style ctx :black)
-    (canvas/fill-rect ctx {:x 0 :y 0 :w 400 :h 400})))
+  (reset! ctx (canvas/get-context el "2d"))
+  (canvas/fill-style @ctx :black)
+  (canvas/fill-rect @ctx {:x 0 :y 0 :w 400 :h 400}))
