f9758ee998f7837b19ac010dcb1d535e7e73c541
Scott Feeney <scott@oceanbase.org>
2013-10-10 13:04:59 -0400

    initial commit

---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject dumbbell "0.1.0-SNAPSHOT"
+  :description "Simple browser game"
+  :license {:name "MIT License"
+            :url "https://github.com/graue/luasynth/blob/master/MIT-LICENSE.txt"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-1913"]
+                 [rm-hull/monet "0.1.8"]]
+  :plugins [[lein-cljsbuild "0.3.3"]]
+  :hooks [leiningen.cljsbuild]
+  :cljsbuild {:builds
+               {:main {:source-paths ["src-cljs"]
+                       :compiler {:output-to "js/main.js"
+                                  :optimizations :whitespace
+                                  :pretty-print true}}}}
+  :source-paths ["no-clj-here"])
---
+++ src-cljs/main.cljs
@@ -0,0 +1,5 @@
+(ns dumbbell.main)
+
+(defn ^:export startgame
+  [el]
+  (.log js/console "Hello world!"))
