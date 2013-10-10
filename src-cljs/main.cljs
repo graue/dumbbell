645a72cdcdccb4ca117e919e437851f0cf7ec39f
Scott Feeney <scott@oceanbase.org>
2013-10-10 13:09:05 -0400

    blank out canvas background

--- src-cljs/main.cljs
+++ src-cljs/main.cljs
@@ -1,5 +1,8 @@
-(ns dumbbell.main)
+(ns dumbbell.main
+  (:require [monet.canvas :as canvas]))
 
 (defn ^:export startgame
   [el]
-  (.log js/console "Hello world!"))
+  (let [ctx (canvas/get-context el "2d")]
+    (canvas/fill-style ctx :black)
+    (canvas/fill-rect ctx {:x 0 :y 0 :w 400 :h 400})))
