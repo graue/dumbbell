951fab9093a2cc4b5abe5ee77c921e1ad2dd79ab
Scott Feeney <scott@oceanbase.org>
2013-10-10 13:27:16 -0400

    add ticker

--- src-cljs/main.cljs
+++ src-cljs/main.cljs
@@ -5,8 +5,22 @@
 (def snake-dir (atom :right))
 (def snake-pos (atom {:x 200 :y 200}))
 
+(defn clear-bg
+  []
+  (canvas/fill-style @ctx :black)
+  (canvas/fill-rect @ctx {:x 0 :y 0 :w 400 :h 400}))
+
+(defn tick
+  []
+  (comment (.log js/console "Tick!"))
+  (.requestAnimationFrame js/window tick))
+
+(defn start-game-loop
+  []
+  (.requestAnimationFrame js/window tick))
+
 (defn ^:export startgame
   [el]
   (reset! ctx (canvas/get-context el "2d"))
-  (canvas/fill-style @ctx :black)
-  (canvas/fill-rect @ctx {:x 0 :y 0 :w 400 :h 400}))
+  (clear-bg)
+  (start-game-loop))
