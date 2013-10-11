c9e6eada3a9d810983461c29105c8d355f5f7894
Scott Feeney <scott@oceanbase.org>
2013-10-10 20:25:48 -0400

    Compute snake position in a neater way
    Possibly overengineered, though.

--- src-cljs/main.cljs
+++ src-cljs/main.cljs
@@ -9,6 +9,26 @@
    37 :left
    39 :right})
 
+(def dir->vek
+  {:up    {:y -1}
+   :down  {:y  1}
+   :left  {:x -1}
+   :right {:x  1}})
+
+(defn mapmap
+  "Return a hashmap like m, but with f applied to each value."
+  ;; XXX: can I use algo.generic.functor from CLJS? This is just fmap
+  [f m]
+  (into {} (for [[k v] m] [k (f v)])))
+
+(defn add-vek
+  "For every key in pos, add the corresponding value from vek,
+   if it exists."
+  [pos vek]
+  (into {}
+    (for [[k v] pos]
+         [k (+ v (get vek k 0))])))
+
 ;; Gameplay constants.
 (def game
   {:w 400 :h 400
@@ -30,23 +50,19 @@
   [ctx {:keys [x y]}]
   (canvas/fill-rect ctx {:x x :y y :w 1 :h 1}))
 
+(defn wrap-around
+  [{:keys [x y]}]
+  {:x (mod x (:w game))
+   :y (mod y (:h game))})
+
 (defn advance-snake
   "Advance the snake one step in direction dir, returning
    an updated pos."
   [pos dir speed]
-  (cond
-    (= dir :left)  {:x (mod (- (:x pos) speed)
-                            (:w game))
-                    :y (:y pos)}
-    (= dir :right) {:x (mod (+ (:x pos) speed)
-                            (:w game))
-                    :y (:y pos)}
-    (= dir :up)    {:y (mod (- (:y pos) speed)
-                            (:h game))
-                    :x (:x pos)}
-    (= dir :down)  {:y (mod (+ (:y pos) speed)
-                            (:h game))
-                    :x (:x pos)}))
+  (wrap-around
+    (add-vek
+      pos
+      (mapmap (partial * speed) (dir->vek dir)))))
 
 (defn update-game-state
   []
