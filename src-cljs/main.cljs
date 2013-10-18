9b92a9eaeabd01693d7814593d3f9dfd0215f9a5
Scott Feeney <scott@oceanbase.org>
2013-10-18 02:25:33 -0400

    Use a protocol and defrecord for 2D vectors

--- src-cljs/main.cljs
+++ src-cljs/main.cljs
@@ -9,25 +9,28 @@
    37 :left
    39 :right})
 
-(def dir->vek
-  {:up    {:y -1}
-   :down  {:y  1}
-   :left  {:x -1}
-   :right {:x  1}})
+(defprotocol VectorMath2D
+  (v+ [v1 v2] "Adds two vectors"))
+
+(defrecord Vec2D [x y]
+  VectorMath2D
+    (v+ [v1 v2]
+      {:x (+ (:x v1)
+             (get v2 :x 0))
+       :y (+ (:y v1)
+             (get v2 :y 0))}))
+
+(def dir->vec2d
+  {:up    (Vec2D.  0 -1)
+   :down  (Vec2D.  0  1)
+   :left  (Vec2D. -1  0)
+   :right (Vec2D.  1  0)})
 
 (defn mapmap
   "Return a hashmap like m, but with f applied to each value."
   ;; XXX: can I use algo.generic.functor from CLJS? This is just fmap
   [f m]
   (into {} (for [[k v] m] [k (f v)])))
-
-(defn add-vek
-  "For every key in pos, add the corresponding value from vek,
-   if it exists."
-  [pos vek]
-  (into {}
-    (for [[k v] pos]
-         [k (+ v (get vek k 0))])))
 
 ;; Gameplay constants.
 (def game
@@ -37,8 +40,8 @@
 
 (def ctx (atom nil))
 (def snake-dir (atom :right))
-(def snake-pos (atom {:x (/ (:w game) 2)
-                      :y (/ (:h game) 2)}))
+(def snake-pos (atom (Vec2D. (/ (:w game) 2))
+                             (/ (:h game) 2)))
 (def snake-speed (atom 0.4))
 (def dumbbell-pos (atom nil))
 
@@ -53,18 +56,18 @@
 
 (defn wrap-around
   [{:keys [x y]}]
-  {:x (mod x (:w game))
-   :y (mod y (:h game))})
+  (Vec2D. (mod x (:w game))
+          (mod y (:h game))))
 
 (def dumbbell-pixels
   (for [y (range -2 3)
         x (range -2 3)
         :when (<= (Math/abs x) (Math/abs y))]
-    {:x x :y y}))
+    (Vec2D. x y)))
 
 (defn- real-dumbbell-pixels
   [dbpos]
-  (map #(wrap-around (add-vek dbpos %)) dumbbell-pixels))
+  (map #(wrap-around (v+ dbpos %)) dumbbell-pixels))
 
 (defn draw-dumbbell
   [pos]
@@ -75,8 +78,8 @@
 (defn place-dumbbell
   []
   (reset! dumbbell-pos
-          {:x (rand-int (:w game))
-           :y (rand-int (:h game))})
+          (Vec2D. (rand-int (:w game))
+                  (rand-int (:h game))))
   (draw-dumbbell @dumbbell-pos))
 
 (defn touching-dumbbell?
@@ -88,22 +91,22 @@
   (canvas/fill-style @ctx (:bg game))
   (doseq [y (range -12 13)
           x (range -12 13)]
-    (put-pixel @ctx (wrap-around (add-vek pos {:x x :y y})))))
+    (put-pixel @ctx (wrap-around (v+ pos (Vec2D. x y))))))
 
 (defn advance-snake
   "Advance the snake one step in direction dir, returning
    an updated pos."
   [pos dir speed]
   (wrap-around
-    (add-vek
+    (v+
       pos
-      (mapmap (partial * speed) (dir->vek dir)))))
+      (mapmap (partial * speed) (dir->vec2d dir)))))
 
 (defn update-game-state
   []
   (swap! snake-pos advance-snake @snake-dir @snake-speed)
-  (let [int-pos {:x (Math/floor (:x @snake-pos))
-                 :y (Math/floor (:y @snake-pos))}]
+  (let [int-pos (Vec2D. (Math/floor (:x @snake-pos))
+                        (Math/floor (:y @snake-pos)))]
     (if (touching-dumbbell? int-pos @dumbbell-pos)
       (do
         (erase-dumbbell @dumbbell-pos)
