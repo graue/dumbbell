1dba8537680b3a67d59fef47d5d4b60103c44869
Scott Feeney <scott@oceanbase.org>
2013-10-19 05:06:25 -0400

    Put argument vectors on same line as defns

--- src-cljs/main.cljs
+++ src-cljs/main.cljs
@@ -54,17 +54,14 @@
 (def snake-speed (atom 0.4))
 (def dumbbell-pos (atom nil))
 
-(defn clear-bg
-  []
+(defn clear-bg []
   (canvas/fill-style @ctx (:bg game))
   (canvas/fill-rect @ctx {:x 0 :y 0 :w (:w game) :h (:h game)}))
 
-(defn put-pixel
-  [ctx {:keys [x y]}]
+(defn put-pixel [ctx {:keys [x y]}]
   (canvas/fill-rect ctx {:x x :y y :w 1 :h 1}))
 
-(defn wrap-around
-  [{:keys [x y]}]
+(defn wrap-around [{:keys [x y]}]
   (Vec2D. (mod x (:w game))
           (mod y (:h game))))
 
@@ -74,29 +71,24 @@
         :when (<= (Math/abs x) (Math/abs y))]
     (Vec2D. x y)))
 
-(defn- real-dumbbell-pixels
-  [dbpos]
+(defn- real-dumbbell-pixels [dbpos]
   (map #(wrap-around (v+ dbpos %)) dumbbell-pixels))
 
-(defn draw-dumbbell
-  [pos]
+(defn draw-dumbbell [pos]
   (canvas/fill-style @ctx (:fg game))
   (doseq [pxpos (real-dumbbell-pixels pos)]
     (put-pixel @ctx pxpos)))
 
-(defn place-dumbbell
-  []
+(defn place-dumbbell []
   (reset! dumbbell-pos
           (Vec2D. (rand-int (:w game))
                   (rand-int (:h game))))
   (draw-dumbbell @dumbbell-pos))
 
-(defn touching-dumbbell?
-  [spos dbpos]
+(defn touching-dumbbell? [spos dbpos]
   (some #{spos} (real-dumbbell-pixels dbpos)))
 
-(defn erase-dumbbell
-  [pos]
+(defn erase-dumbbell [pos]
   (canvas/fill-style @ctx (:bg game))
   (doseq [y (range -12 13)
           x (range -12 13)]
@@ -111,8 +103,7 @@
       pos
       (mapmap (partial * speed) (dir->vec2d dir)))))
 
-(defn update-game-state
-  []
+(defn update-game-state []
   (let [old-pix-pos (vfloor @snake-pos)
         pos (swap! snake-pos advance-snake @snake-dir @snake-speed)
         pix-pos (vfloor pos)]
@@ -133,23 +124,19 @@
             (canvas/fill-style @ctx (:fg game))
             (put-pixel @ctx pix-pos))))))
 
-(defn tick
-  []
+(defn tick []
   (update-game-state)
   (.requestAnimationFrame js/window tick))
 
-(defn start-game-loop
-  []
+(defn start-game-loop []
   (.requestAnimationFrame js/window tick))
 
-(defn process-keydown
-  [ev]
+(defn process-keydown [ev]
   (when-let [dir (keycode->dir (.-keyCode ev))]
     (when (not-any? #{#{dir @snake-dir}} opposite-dirs)
       (reset! snake-dir dir))))
 
-(defn ^:export startgame
-  [el]
+(defn ^:export startgame [el]
   (reset! ctx (canvas/get-context el "2d"))
   (clear-bg)
   (place-dumbbell)
